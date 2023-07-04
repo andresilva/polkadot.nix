8e4ef668cf9c146f7a015ff0b43de629c87ba4d7
André Silva <andrerfosilva@gmail.com>
2023-07-04 12:02:46 +0100

    shell: init

--- shell.nix
+++ shell.nix
@@ -1,5 +1,33 @@
 { pkgs }:
 
-pkgs.mkShell {
-  buildInputs = [ pkgs.hello ];
+let
+  rust-toolchain = with pkgs.fenix; combine [
+    (stable.withComponents [
+      "cargo"
+      "clippy"
+      "rust-analyzer"
+      "rust-src"
+      "rustc"
+    ])
+    (latest.withComponents [ "rustfmt" ])
+    targets.wasm32-unknown-unknown.stable.rust-std
+  ];
+  mold = pkgs.wrapBintoolsWith { bintools = pkgs.mold; };
+in
+with pkgs; mkShell {
+  buildInputs = [
+    clang
+    openssl
+    pkg-config
+    rust-toolchain
+  ];
+
+  # use mold as linker on linux x86_64
+  CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "clang";
+  CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS = "-C link-arg=-fuse-ld=${mold}/bin/ld.mold";
+
+  LIBCLANG_PATH = lib.makeLibraryPath [ llvmPackages.libclang ];
+  PROTOC = "${lib.makeBinPath [ protobuf ]}/protoc";
+  ROCKSDB_LIB_DIR = lib.makeLibraryPath [ rocksdb ];
+  RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
 }
