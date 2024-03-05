9aa2e795d7f4cf11986face06e3bb82cace07706
André Silva <andrerfosilva@gmail.com>
2024-03-05 16:41:46 +0000

    subkey: unstable-2023-09-28 -> unstable-2024-03-05

--- pkgs/subkey/default.nix
+++ pkgs/subkey/default.nix
@@ -7,21 +7,24 @@
 
 rustPlatform.buildRustPackage {
   pname = "subkey";
-  version = "unstable-2023-09-28";
+  version = "unstable-2024-03-05";
 
   src = fetchFromGitHub {
     owner = "paritytech";
     repo = "polkadot-sdk";
-    rev = "de71fecc4e58d99474ff655789801e5edf3764b1";
-    sha256 = "0qj0n6lq6j0f0ihldfh7kxwd8r6789jr7w1dvja9gdfxx1ir8ff1";
+    rev = "c367ac2488db23a68e1cdf446cece58c66ea93d5";
+    sha256 = "1s3545sidp4zrq558vf6g27izk5c78m6hi77pi6354pjkw222mi3";
   };
 
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "ark-secret-scalar-0.0.2" = "sha256-Tcrz2tT561ICAJzMgarSTOnaUEPeTFKZzE7rkdL3eUQ=";
-      "common-0.1.0" = "sha256-dnZKDx3Rw5cd4ejcilo3Opsn/1XK9yWGxhceuwvBE0o=";
-      "fflonk-0.1.0" = "sha256-MNvlePHQdY8DiOq6w7Hc1pgn7G58GDTeghCKHJdUy7E=";
+      "ark-secret-scalar-0.0.2" = "sha256-91sODxaj0psMw0WqigMCGO5a7+NenAsRj5ZmW6C7lvc=";
+      "common-0.1.0" = "sha256-LHz2dK1p8GwyMimlR7AxHLz1tjTYolPwdjP7pxork1o=";
+      "fflonk-0.1.0" = "sha256-+BvZ03AhYNP0D8Wq9EMsP+lSgPA6BBlnWkoxTffVLwo=";
+      "sp-ark-bls12-381-0.4.2" = "sha256-nNr0amKhSvvI9BlsoP+8v6Xppx/s7zkf0l9Lm3DW8w8=";
+      "sp-crypto-ec-utils-0.4.1" = "sha256-KXyG43YIzMG2r6kCTyQyCIHSAkXSlZv8pbFEXXvC4JU=";
+      "sp-debug-derive-8.0.0" = "sha256-/Sw1ZM/JcJBokFE4y2mv/P43ciTL5DEm0PDG0jZvMkI=";
     };
   };
 
