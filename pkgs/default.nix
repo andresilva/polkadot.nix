39976362777f2ca731d6433bb8254e39a66337e0
André Silva <andrerfosilva@gmail.com>
2024-03-05 17:04:36 +0000

    subalfred: init

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,6 +5,7 @@
     inherit (pkgs.darwin.apple_sdk.frameworks) Security SystemConfiguration;
   };
   srtool-cli = pkgs.callPackage ./srtool-cli { };
+  subalfred = pkgs.callPackage ./subalfred { };
   subkey = pkgs.callPackage ./subkey { };
   subrpc = pkgs.callPackage ./subrpc { };
   subwasm = pkgs.callPackage ./subwasm { };
---
+++ pkgs/subalfred/default.nix
@@ -0,0 +1,38 @@
+{ lib
+, fetchFromGitHub
+, openssl
+, pkg-config
+, rustPlatform
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "subalfred";
+  version = "0.9.3";
+
+  src = fetchFromGitHub {
+    owner = "hack-ink";
+    repo = "subalfred";
+    rev = "v${version}";
+    hash = "sha256-YQiSZcyKzn+43+vgNdg6fkKFpwlG1qso1lsoTq2X58w=";
+  };
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "subcryptor-0.10.2" = "sha256-lX+EXUsEha1wT9OeQezxnB6DdTWBNViLZYSddONCktg=";
+    };
+  };
+
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ openssl ];
+
+  doCheck = false;
+
+  meta = with lib; {
+    description = "An All-In-One Substrate Development Toolbox";
+    homepage = "https://github.com/hack-ink/subalfred";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ andresilva ];
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+}
