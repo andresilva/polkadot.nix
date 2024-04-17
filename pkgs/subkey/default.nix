9efa4e557cc835af3587b3945aa4d7d4cc7a6cb8
André Silva <andrerfosilva@gmail.com>
2024-04-17 12:19:36 +0100

    subkey: unstable-2024-03-05 -> unstable-2024-04-17

--- pkgs/subkey/default.nix
+++ pkgs/subkey/default.nix
@@ -1,5 +1,7 @@
 { lib
 , fetchFromGitHub
+, openssl
+, pkg-config
 , protobuf
 , rocksdb
 , rustPlatform
@@ -7,13 +9,13 @@
 
 rustPlatform.buildRustPackage {
   pname = "subkey";
-  version = "unstable-2024-03-05";
+  version = "unstable-2024-04-17";
 
   src = fetchFromGitHub {
     owner = "paritytech";
     repo = "polkadot-sdk";
-    rev = "c367ac2488db23a68e1cdf446cece58c66ea93d5";
-    sha256 = "1s3545sidp4zrq558vf6g27izk5c78m6hi77pi6354pjkw222mi3";
+    rev = "e6f3106d894277deba043a83e91565de24263a1b";
+    sha256 = "1nw59p49zww2w9w3ng9xfm7173b8p4z1d3193ax20g5vscr6dhsh";
   };
 
   cargoLock = {
@@ -22,16 +24,18 @@ rustPlatform.buildRustPackage {
       "ark-secret-scalar-0.0.2" = "sha256-91sODxaj0psMw0WqigMCGO5a7+NenAsRj5ZmW6C7lvc=";
       "common-0.1.0" = "sha256-LHz2dK1p8GwyMimlR7AxHLz1tjTYolPwdjP7pxork1o=";
       "fflonk-0.1.0" = "sha256-+BvZ03AhYNP0D8Wq9EMsP+lSgPA6BBlnWkoxTffVLwo=";
+      "litep2p-0.3.0" = "sha256-IiJmmSb1+8+HbT/LP/zvhioVBeeGAncf4zo7Czuq6qY=";
       "sp-ark-bls12-381-0.4.2" = "sha256-nNr0amKhSvvI9BlsoP+8v6Xppx/s7zkf0l9Lm3DW8w8=";
-      "sp-crypto-ec-utils-0.4.1" = "sha256-KXyG43YIzMG2r6kCTyQyCIHSAkXSlZv8pbFEXXvC4JU=";
-      "sp-debug-derive-8.0.0" = "sha256-/Sw1ZM/JcJBokFE4y2mv/P43ciTL5DEm0PDG0jZvMkI=";
+      "sp-crypto-ec-utils-0.4.1" = "sha256-/Sw1ZM/JcJBokFE4y2mv/P43ciTL5DEm0PDG0jZvMkI=";
     };
   };
 
-  nativeBuildInputs = [ rustPlatform.bindgenHook ];
+  nativeBuildInputs = [ pkg-config rustPlatform.bindgenHook ];
+  buildInputs = [ openssl ];
 
   buildAndTestSubdir = "substrate/bin/utils/subkey";
 
+  OPENSSL_NO_VENDOR = 1;
   PROTOC = "${lib.makeBinPath [ protobuf ]}/protoc";
   ROCKSDB_LIB_DIR = lib.makeLibraryPath [ rocksdb ];
 
