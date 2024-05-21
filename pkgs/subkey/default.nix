f4a49cdc2aea01eafd83d1668b265ba352aa83d5
André Silva <andrerfosilva@gmail.com>
2024-05-21 19:22:52 +0100

    subkey: unstable-2024-04-17 -> polkadot-v1.12.0

--- pkgs/subkey/default.nix
+++ pkgs/subkey/default.nix
@@ -7,24 +7,25 @@
 , rustPlatform
 }:
 
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "subkey";
-  version = "unstable-2024-04-17";
+  version = "polkadot-v1.12.0";
 
   src = fetchFromGitHub {
     owner = "paritytech";
     repo = "polkadot-sdk";
-    rev = "e6f3106d894277deba043a83e91565de24263a1b";
-    sha256 = "1nw59p49zww2w9w3ng9xfm7173b8p4z1d3193ax20g5vscr6dhsh";
+    rev = version;
+    hash = "sha256-/m7Tg+9JHbnwKwWPY8gWIJkIHktGFlqcrbLLgNWjfwU=";
   };
 
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
       "ark-secret-scalar-0.0.2" = "sha256-91sODxaj0psMw0WqigMCGO5a7+NenAsRj5ZmW6C7lvc=";
+      "ckb-merkle-mountain-range-0.6.0" = "sha256-oTe1l406lTpgOefPai664JYwzezLjkIDXpiZTfjbd28=";
       "common-0.1.0" = "sha256-LHz2dK1p8GwyMimlR7AxHLz1tjTYolPwdjP7pxork1o=";
       "fflonk-0.1.0" = "sha256-+BvZ03AhYNP0D8Wq9EMsP+lSgPA6BBlnWkoxTffVLwo=";
-      "litep2p-0.3.0" = "sha256-IiJmmSb1+8+HbT/LP/zvhioVBeeGAncf4zo7Czuq6qY=";
+      "litep2p-0.3.0" = "sha256-y0my2vi0+2CWNOtCh/vtsUbIcU1iNSFAJbLiCktEcOc=";
       "sp-ark-bls12-381-0.4.2" = "sha256-nNr0amKhSvvI9BlsoP+8v6Xppx/s7zkf0l9Lm3DW8w8=";
       "sp-crypto-ec-utils-0.4.1" = "sha256-/Sw1ZM/JcJBokFE4y2mv/P43ciTL5DEm0PDG0jZvMkI=";
     };
@@ -34,6 +35,8 @@ rustPlatform.buildRustPackage {
   buildInputs = [ openssl ];
 
   buildAndTestSubdir = "substrate/bin/utils/subkey";
+
+  doCheck = false;
 
   OPENSSL_NO_VENDOR = 1;
   PROTOC = "${lib.makeBinPath [ protobuf ]}/protoc";
