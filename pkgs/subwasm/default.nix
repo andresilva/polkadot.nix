7243bd5909948a1a9bcf2099aa53bd5ddd063d83
André Silva <andrerfosilva@gmail.com>
2024-05-21 18:49:09 +0100

    subwasm: 0.20.0 -> 0.21.1

--- pkgs/subwasm/default.nix
+++ pkgs/subwasm/default.nix
@@ -5,23 +5,18 @@
 , rustPlatform
 }:
 
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "subwasm";
-  version = "0.20.0";
+  version = "0.21.1";
 
   src = fetchFromGitHub {
-    owner = "andresilva";
+    owner = "chevdor";
     repo = "subwasm";
-    rev = "08b9496e9230e410593977dd2396b0917f028f62"; # v${version}
-    hash = "sha256-Kq/W2knfI2sZ96Uqqv/Fj1v47uVL6i4RpWaQa96w728=";
+    rev = "v${version}";
+    hash = "sha256-Vxy1l2i+upLHwytsfVjV8IOUC3S0d5kHcS/JtKr7K0U=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "sc-allocator-4.1.0-dev" = "sha256-QPil2OAH3Oo1ZBlCGygCvdi6jbvkwQRl0GC7ogTw7nI=";
-    };
-  };
+  cargoHash = "sha256-Cjxqs2bUjepB4eH/3m18+toXIhQHUoNUTaNdg7syHeE=";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ];
