002ca60e38fa2bb5d4c48713be5f7e3ab2025aae
André Silva <andrerfosilva@gmail.com>
2024-05-22 15:03:47 +0100

    subwasm: 0.21.1 -> 0.21.2

--- pkgs/subwasm/default.nix
+++ pkgs/subwasm/default.nix
@@ -7,16 +7,34 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "subwasm";
-  version = "0.21.1";
+  version = "0.21.2";
 
   src = fetchFromGitHub {
     owner = "chevdor";
     repo = "subwasm";
     rev = "v${version}";
-    hash = "sha256-Vxy1l2i+upLHwytsfVjV8IOUC3S0d5kHcS/JtKr7K0U=";
+    hash = "sha256-ayD1exyU0HTc13UD1oLJdhZPLqgBPLfZQSKajmn7s0I=";
+
+    # the build process of subwasm requires a .git folder in order to determine
+    # the git commit hash that is being built and add it to the version string.
+    # since having a .git folder introduces reproducibility issues to the nix
+    # build, we check the git commit hash after fetching the source and save it
+    # into a .git_commit file, and then delete the .git folder. we can then use
+    # this file to populate an environment variable with the commit hash, which
+    # is picked up by subwasm's build process.
+    leaveDotGit = true;
+    postFetch = ''
+      ( cd $out; git rev-parse --short HEAD > .git_commit )
+      rm -rf $out/.git
+    '';
   };
 
-  cargoHash = "sha256-Cjxqs2bUjepB4eH/3m18+toXIhQHUoNUTaNdg7syHeE=";
+  preBuild = ''
+    export SUBWASM_CLI_GIT_COMMIT_HASH=$(< .git_commit)
+    rm .git_commit
+  '';
+
+  cargoHash = "sha256-gfmhnFIzhYBS5GaiiqI2Hvbq+dygHlV5Ni/SW3D6ljc=";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ];
