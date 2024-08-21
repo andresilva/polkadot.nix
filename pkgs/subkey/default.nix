bb2fae2c613d6269c70ba702c2fe2e1a6ebdb2fa
André Silva <andrerfosilva@gmail.com>
2024-08-21 17:33:57 +0100

    subkey: polkadot-v1.13.0 -> polkadot-stable2407-1

--- pkgs/subkey/default.nix
+++ pkgs/subkey/default.nix
@@ -9,24 +9,19 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "subkey";
-  version = "polkadot-v1.13.0";
+  version = "stable2407-1";
 
   src = fetchFromGitHub {
     owner = "paritytech";
     repo = "polkadot-sdk";
-    rev = version;
-    hash = "sha256-9iJRkaI8J1jjrM37ol/qPxZwTgLvMPrTRMR+gpL90Yg=";
+    rev = "polkadot-${version}";
+    hash = "sha256-WEH0ICe66AbhNufqZz2kWCdV8UVIxjdHUPIJ1A44e6Y=";
   };
 
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "ark-secret-scalar-0.0.2" = "sha256-91sODxaj0psMw0WqigMCGO5a7+NenAsRj5ZmW6C7lvc=";
-      "common-0.1.0" = "sha256-LHz2dK1p8GwyMimlR7AxHLz1tjTYolPwdjP7pxork1o=";
-      "fflonk-0.1.0" = "sha256-+BvZ03AhYNP0D8Wq9EMsP+lSgPA6BBlnWkoxTffVLwo=";
       "simple-mermaid-0.1.0" = "sha256-IekTldxYq+uoXwGvbpkVTXv2xrcZ0TQfyyE2i2zH+6w=";
-      "sp-ark-bls12-381-0.4.2" = "sha256-nNr0amKhSvvI9BlsoP+8v6Xppx/s7zkf0l9Lm3DW8w8=";
-      "sp-crypto-ec-utils-0.4.1" = "sha256-/Sw1ZM/JcJBokFE4y2mv/P43ciTL5DEm0PDG0jZvMkI=";
     };
   };
 
