781c1db5732496b1572f5084f2ad8ee29c9b195f
André Silva <andrerfosilva@gmail.com>
2024-10-22 16:27:41 +0100

    graypaper: fix bibliography references

--- pkgs/graypaper/default.nix
+++ pkgs/graypaper/default.nix
@@ -26,10 +26,14 @@ runCommand "graypaper"
     cp -r $src/* $TMP
     cd $TMP
 
-    xelatex -halt-on-error graypaper.tex
+    xelatex -halt-on-error graypaper
+    biber graypaper
+    xelatex -halt-on-error graypaper
     mv graypaper.pdf $out/share/doc/graypaper/graypaper-${version}.pdf
 
     patch -p1 < ${./printer-friendly.patch}
-    xelatex -halt-on-error graypaper.tex
+    xelatex -halt-on-error graypaper
+    biber graypaper
+    xelatex -halt-on-error graypaper
     mv graypaper.pdf $out/share/doc/graypaper/graypaper-${version}-printer-friendly.pdf
   ''
