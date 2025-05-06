b55435c606560aad68cac43b3505ad908b354b90
Robert Hambrock <roberthambrock@gmail.com>
2025-05-06 19:13:49 +0200

    polkadot-sdk: 2503 -> 2503-1

--- pkgs/polkadot-sdk/generic.nix
+++ pkgs/polkadot-sdk/generic.nix
@@ -25,13 +25,13 @@ in
 rustPlatform.buildRustPackage rec {
   inherit pname;
 
-  version = "2503";
+  version = "2503-1";
 
   src = fetchFromGitHub {
     owner = "paritytech";
     repo = "polkadot-sdk";
     rev = "polkadot-stable${version}";
-    hash = "sha256-nPZFmsf82JpBrOrErH5hrEcmieECfgA7JWzEyEh8AAE=";
+    hash = "sha256-vbmj+BIoSGEcSMUfE9/kFny9Dc8Oy5VMVCKbP578ub4=";
 
     # the build process of polkadot requires a .git folder in order to determine
     # the git commit hash that is being built and add it to the version string.
@@ -53,7 +53,7 @@ rustPlatform.buildRustPackage rec {
   '';
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-yOJyvpsEK4Ab/Bh6xmqAEHhj1Rq4u/CevcP7vJi0zxo=";
+  cargoHash = "sha256-+wLh5g84g1DyP1c1RSqigHPPEiBe0d0g3UT1atImuow=";
 
   buildType = "production";
   buildAndTestSubdir = target;
