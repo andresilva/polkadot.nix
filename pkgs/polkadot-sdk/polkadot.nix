82fc7d3ee4048cb6604edb296ee353a47eb1a8c5
André Silva <andrerfosilva@gmail.com>
2025-12-22 12:40:28 +0000

    polkadot-sdk: 2509-3 -> 2512

--- pkgs/polkadot-sdk/polkadot.nix
+++ pkgs/polkadot-sdk/polkadot.nix
@@ -1,6 +1,7 @@
 {
   callPackage,
   lib,
+  rust-jemalloc-sys-unprefixed,
 }:
 
 (callPackage ./generic.nix {
@@ -8,7 +9,9 @@
   target = "polkadot";
   description = "Implementation of a https://polkadot.network node in Rust based on the Substrate framework";
   license = lib.licenses.gpl3Only;
+  rust-jemalloc-sys = rust-jemalloc-sys-unprefixed;
 }).overrideAttrs
   (_: {
     doCheck = false;
+    cargoBuildFlags = [ "--features=jemalloc-allocator" ];
   })
