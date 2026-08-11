96c5c3eba91b310e52815b28c9e0fa12113cde4d
André Silva <andrerfosilva@gmail.com>
2026-08-11 13:21:06 +0100

    shell: use llvmPackages_latest

--- shell.nix
+++ shell.nix
@@ -38,13 +38,13 @@ let
       rustTarget = stdenv.hostPlatform.rust.cargoEnvVarTarget;
       mkLinkerFlags = ld: "-Clink-arg=-fuse-ld=${ld} -Clink-arg=-Wl,--no-rosegment -Clink-arg=-flto";
       linkerFlags = {
-        lld = mkLinkerFlags "${llvmPackages.lld}/bin/ld.lld";
+        lld = mkLinkerFlags "${llvmPackages_latest.lld}/bin/ld.lld";
         mold = mkLinkerFlags "${mold}/bin/ld.mold";
         wild = mkLinkerFlags "${wild}/bin/ld.wild";
       };
       rustflags =
         if stdenv.isDarwin then
-          "-Clink-arg=-fuse-ld=${llvmPackages.lld}/bin/ld64.lld"
+          "-Clink-arg=-fuse-ld=${llvmPackages_latest.lld}/bin/ld64.lld"
         else
           linkerFlags.${linker};
     in
@@ -54,11 +54,11 @@ let
     };
 in
 with pkgs;
-mkShell.override { stdenv = clangStdenv; } {
+mkShell.override { stdenv = llvmPackages_latest.stdenv; } {
   packages =
     packages
     ++ [
-      llvmPackages.lld
+      llvmPackages_latest.lld
       openssl
       pkg-config
       rust-toolchain
@@ -68,7 +68,7 @@ mkShell.override { stdenv = clangStdenv; } {
     ];
 
   env = {
-    LIBCLANG_PATH = lib.makeLibraryPath [ llvmPackages.libclang ];
+    LIBCLANG_PATH = lib.makeLibraryPath [ llvmPackages_latest.libclang ];
     RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
 
     OPENSSL_NO_VENDOR = 1;
